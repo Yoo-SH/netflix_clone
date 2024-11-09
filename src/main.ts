41a97072d3e58d4a9523dab6b52039c2473cd2c4
Yoo-SH <ads137485@jbnu.ac.kr>
2024-11-10 02:13:33 +0900

    init: custom toast 라이브러리 구축 및 세팅

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import Toast from 'vue-toastification'
+
+const options = {
+    position: 'top-right',
+    timeout: 0, // 자동으로 사라지지 않도록 설정
+    closeOnClick: true,
+  };
+  
 
 const pinia = createPinia() // 전역 상태 관리 라이브러리인 Pinia를 생성합니다.
-createApp(App).use(router).use(pinia).mount('#app')
+createApp(App).use(router).use(pinia).use(Toast,options).mount('#app')
